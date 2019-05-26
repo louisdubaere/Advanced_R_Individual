871bfa413282b666afc3b61196593e774b17c2cf
Louis Dubaere <dubaerelouis@gmail.com>
2019-05-26 18:18:57 +0200

    Changes to report

--- load_functions.R
+++ load_functions.R
@@ -9,7 +9,6 @@ numerical_columns <- function(data) {
   return(nums)
 }
 
-
 drop_columns <- function(data, columns) {
   copy <- data.frame(data)
   return( copy[ , -which(names(copy) %in% columns)])
@@ -33,11 +32,23 @@ split_train_test <- function(df, train=0.8, seed=2112) {
   return(mylist)
 }
 
+mape<-function(real,predicted){
+  return(mean(abs((real-predicted)/real)))
+}
+
 calculate_MAPE_lm <- function(data) {
   splitted <- split_train_test(data)
   lm <- lm(price~., data = splitted$Train)
   p <- predict(lm, splitted$Test)
-  MAPE <- MAPE(y_pred = p, y_true = splitted$Test$price)
+  MAPE <- mape(predicted = p, real = splitted$Test$price)
+  return(MAPE)
+}
+
+calculate_MAPE_dt <- function(data) {
+  splitted <- split_train_test(data)
+  fit <- rpart(price~., data = splitted$Train)
+  p <- predict(fit, splitted$Test)
+  MAPE <- mape(predicted = p, real = splitted$Test$price)
   return(MAPE)
 }
 
@@ -45,6 +56,41 @@ calculate_MAPE_rf <- function(data) {
   splitted <- split_train_test(data)
   rf <- randomForest(price~., data = splitted$Train)
   p <- predict(rf, splitted$Test)
-  MAPE <- MAPE(y_pred = p, y_true = splitted$Test$price)
+  MAPE <- mape(predicted = p, real = splitted$Test$price)
   return(MAPE)
+}
+
+get_train_data_MAPE_lm <- function(data, train_n, price) {
+  train <- cbind(data[1:train_n,], price)
+  
+  train <-train[train$grade != "3",]
+  train <-train[train$bathrooms != "5.75",]
+  
+  #colnames(train)[20] <- "price"
+  calculate_MAPE_lm(train)
+}
+
+get_train_data_MAPE_rf <- function(data, train_n, price) {
+  train <- cbind(data[1:train_n,], price)
+  
+  train <-train[train$grade != "3",]
+  train <-train[train$bathrooms != "5.75",]
+  
+  #colnames(train)[20] <- "price"
+  calculate_MAPE_rf(train)
+}
+
+predict_house_prices_lm <- function(data, train_n, price) {
+  train <- cbind(data[1:train_n,], price)
+  test <- data[(train_n+1):nrow(data),]
+  
+  lm <- lm(price~., data = train)
+  
+  test[test$bathrooms == "5.5",]$bathrooms <- "5.25"
+  test[test$bathrooms == "6.75",]$bathrooms <- "6"
+  test[test$grade == "13",]$grade <- "12"
+  
+  p <- predict(lm, test)
+  
+  return(p)
 }
